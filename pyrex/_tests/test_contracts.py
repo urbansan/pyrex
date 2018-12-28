973b2410dd99e109568fa722b8dc1ec13b95ac5a
urban <urbansanek@gmail.com>
2018-12-28 01:10:43 +0100

    tests: contract and trade tests corrected

--- pyrex/_tests/test_contracts.py
+++ pyrex/_tests/test_contracts.py
@@ -1,26 +1,27 @@
 import os
 import sys
 import unittest
+from datetime import date, timedelta
 sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
-from pyrex.classes.contracts import ContractFactory, Contract, contract_typologies
+from pyrex.classes.contracts import get_contract, Contract, contract_typologies
 
 
 class TestContractFactory(unittest.TestCase):
 
     def test_spot(self):
-        c1 = ContractFactory('spot', nominal=1000, currency='eur')
+        c1 = get_contract('spot','eur', 'pln', 1000, 5000)
         self.assertIsInstance(c1, Contract)
         self.assertIsInstance(c1, contract_typologies.Spot)
         self.assertEqual(len(c1.trades), 1)
 
     def test_outright(self):
-        c1 = ContractFactory('outright', nominal=1000, currency='eur')
+        c1 = get_contract('outright', 'eur', 'pln', 1000, 5000, date.today())
         self.assertIsInstance(c1, Contract)
         self.assertIsInstance(c1, contract_typologies.Outright)
         self.assertEqual(len(c1.trades), 1)
 
     def test_fx_swap(self):
-        c1 = ContractFactory('fx_swap', nominal=1000, currency='eur')
+        c1 = get_contract('fx_swap','eur', 'pln', 1000, 5000, date.today() + timedelta(days=40))
         self.assertIsInstance(c1, Contract)
         self.assertIsInstance(c1, contract_typologies.FxSwap)
         self.assertEqual(len(c1.trades), 2)
@@ -28,7 +29,7 @@ class TestContractFactory(unittest.TestCase):
 
     def test_raises(self):
         with self.assertRaises(ValueError):
-            ContractFactory('typology does not exist')
+            get_contract('typology does not exist')
 
 if __name__ == '__main__':
     unittest.main()
